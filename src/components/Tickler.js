153f2137ab008009cd284c63d3d1603a5f77316f
Clinton <+>
2023-08-17 13:55:38 +0300

    completed assignment

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -4,8 +4,7 @@ function Tickler() {
   function tickle() {
     console.log("Teehee!");
   }
-
-  return <button>Tickle me!</button>;
+return <button onClick={tickle}>Tickle me!</button>;
 }
 
 export default Tickler;
